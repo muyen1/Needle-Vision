1aafe31552c1f819f905757d7e981e73812bd636
TomasQuat <tquatito@gmail.com>
2020-11-17 23:49:17 -0800

    Camera functionality, Social Media skeleton complete

--- app/src/main/java/com/example/needlevision/MainActivity.java
+++ app/src/main/java/com/example/needlevision/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.needlevision;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.needlevision.adapters.SlidePagerAdapter;
 import com.example.needlevision.fragments.map_fragment;
@@ -24,7 +34,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
     private PagerAdapter pagerAdapter;
 
     private static final int RC_SIGN_IN = 1;
+    private static final int CAMERA_PERM_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+    private static final int PHOTO_REQUEST_CODE = 103;
+    private String photoPath;
     GoogleSignInClient mGoogleSignInClient;
     SignInButton signInButton;
 
@@ -95,8 +113,59 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Bring Up Camera", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                checkCameraPermissions();
             }
         });
+    }
+
+    private void checkCameraPermissions() {
+        // Display camera permission prompt
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA }, CAMERA_PERM_CODE);
+        } else {
+            //Open Camera
+            openCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (camera.resolveActivity(this.getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = createImageFile();
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.FileProvider", photoFile);
+                camera.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(camera, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+    // Create Image File
+    public File createImageFile() {
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String imageName = timestamp + "_";
+        File storageDirectory = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+
+        try {
+            image = File.createTempFile(imageName, ".jpg", storageDirectory);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        photoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void signIn() {
@@ -114,6 +183,18 @@ public class MainActivity extends AppCompatActivity {
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+        } else if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                File nullPhoto = new File(photoPath);
+                nullPhoto.delete();
+            } else {
+                Intent photoActivity = new Intent(MainActivity.this, PhotoActivity.class);
+                photoActivity.putExtra("PHOTO_PATH", photoPath);
+                startActivityForResult(photoActivity, PHOTO_REQUEST_CODE);
+            }
+        } else if (requestCode == PHOTO_REQUEST_CODE) {
+            //Return from the photo activity
+            //Refresh map pins/list view entries?
         }
     }
 
@@ -131,5 +212,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    private void socialMediaUpload() {
+        Intent mediaUpload = new Intent();
+        mediaUpload.setAction(Intent.ACTION_SEND);
+        mediaUpload.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(this, "com.example.android.FileProvider", new File(photoPath)));
+        mediaUpload.setType("image/jpg");
+        this.startActivity(Intent.createChooser(mediaUpload, this.getResources().getText(R.string.send_to)));
+    }
 }
