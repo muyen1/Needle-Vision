2d607bfe645c1780a057e89cf85a25b22f9745db
TomasQuat <tquatito@gmail.com>
2020-11-18 22:46:27 -0800

    Functionality: Automated writing to database

--- app/src/main/java/com/example/needlevision/PhotoActivity.java
+++ app/src/main/java/com/example/needlevision/PhotoActivity.java
@@ -1,18 +1,33 @@
 package com.example.needlevision;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class PhotoActivity extends AppCompatActivity {
+import com.example.needlevision.service.Database;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class PhotoActivity extends AppCompatActivity implements LocationListener {
 
     private String photoPath;
     private ImageButton btn_back;
@@ -25,10 +40,21 @@ public class PhotoActivity extends AppCompatActivity {
     private TextView tv_location;
     private EditText et_description;
 
+    private Database db;
+
+    private Calendar calendar;
+    private String date;
+    private double latitude;
+    private double longitude;
+
+    private LocationManager lm;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        db = new Database();
 
         btn_back = findViewById(R.id.btn_back);
         btn_upload = findViewById(R.id.btn_upload);
@@ -42,7 +68,29 @@ public class PhotoActivity extends AppCompatActivity {
         photoPath = getIntent().getStringExtra("PHOTO_PATH");
         setPicture(photoPath);
 
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+        calendar = Calendar.getInstance();
+        date = df.format(calendar.getTime());
+        tv_date.setText(date);
 
+        initLocation();
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) this,
+                    new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+        }
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
+        String coordinates = Double.toString(latitude) + ", " + Double.toString(longitude);
+        tv_location.setText(coordinates);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,10 +105,32 @@ public class PhotoActivity extends AppCompatActivity {
         btn_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String description = et_description.getText().toString();
+                db.upload(photoPath, description, date, latitude, longitude);
                 setResult(RESULT_OK);
                 finish();
             }
         });
+    }
+
+    public void initLocation(){
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) this, new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION },
+                    1);
+        }
+    }
+
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+        }
+    }
+
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Photo GPS data not saved, location services disabled",
+                Toast.LENGTH_LONG).show();
     }
 
     // Set the current picture
