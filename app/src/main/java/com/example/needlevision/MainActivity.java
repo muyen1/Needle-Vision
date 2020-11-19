b93fe8986d4cf2654c310cc5a5c32058deda784b
marvin <36011565+marvinngo@users.noreply.github.com>
2020-11-18 19:18:06 -0800

    firebase worknig removed postloginactivity, login takes user to map

--- app/src/main/java/com/example/needlevision/MainActivity.java
+++ app/src/main/java/com/example/needlevision/MainActivity.java
@@ -2,27 +2,15 @@ package com.example.needlevision;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.needlevision.adapters.SlidePagerAdapter;
-import com.example.needlevision.fragments.map_fragment;
-import com.example.needlevision.fragments.posts_fragment;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -30,29 +18,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.Calendar;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 1;
@@ -63,13 +50,19 @@ public class MainActivity extends AppCompatActivity {
     private GoogleMap mMap;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference mDatabaseReference;
     private String photoPath;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    UploadTask uploadTask;
 
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        calendar = Calendar.getInstance();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         // Configure sign-in to request the user's ID, email address, and basic
@@ -81,10 +74,10 @@ public class MainActivity extends AppCompatActivity {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         mAuth = FirebaseAuth.getInstance();
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         // Read from the database upon adding new entry (UPDATE POSTINGS HERE)
-        mDatabaseReference.child("posts").addValueEventListener(new ValueEventListener() {
+        mDatabase.child("posts").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> postChildren = dataSnapshot.getChildren();
@@ -152,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
+
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
@@ -175,15 +168,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             firebaseAuthWithGoogle(account.getIdToken());
-
-
-//            // Signed in successfully, show authenticated UI.
-
-            loadPager();
-            // Signed in successfully, show authenticated UI.
-
-//            Intent intent = new Intent(MainActivity.this, PostLoginActivity.class);
-//            startActivity(intent);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -202,8 +186,8 @@ public class MainActivity extends AppCompatActivity {
                             Log.i("Success", "signInAnonymously:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             // Signed in successfully, show authenticated UI.
-                            Intent intent = new Intent(MainActivity.this, PostLoginActivity.class);
-                            startActivity(intent);
+                            loadPager();
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("Error", "signInAnonymously:failure", task.getException());
@@ -228,8 +212,8 @@ public class MainActivity extends AppCompatActivity {
                             Log.d("Success", "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             // Signed in successfully, show authenticated UI.
-                            Intent intent = new Intent(MainActivity.this, PostLoginActivity.class);
-                            startActivity(intent);
+                            loadPager();
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("Error", "signInWithCredential:failure", task.getException());
@@ -239,6 +223,87 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+//    private void signOut() {
+//        FirebaseAuth.getInstance().signOut();
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Toast.makeText(PostLoginActivity.this, "Signed out successfully", Toast.LENGTH_LONG).show();
+//                        finish();
+//                    }
+//                });
+//    }
+    //private void upload(String path)
+    private void upload(String path){
+        final String randomKey = UUID.randomUUID().toString();
+        final StorageReference imageRef = storageRef.child("/images/" + randomKey);
+        Uri file = Uri.fromFile(new File(path));
+        uploadTask = imageRef.putFile(file);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return imageRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                    date = dateFormat.format(calendar.getTime());
+                    //INSERT STRINGS TO POST BELOW
+                    //writeNewPost(getFirebaseUserID(), "description string", "status string", date, 123, 321, downloadUri.toString());
+                    Log.i("url", downloadUri.toString());
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+        });
+    }
+
+    private void writeNewPost(String userID, String description, String status, String date, double latitude, double longitude, String imageURL){
+        Post newPost = new Post(userID, description, status, date, latitude, longitude, imageURL);
+
+        String key = mDatabase.child("posts").push().getKey();
+        mDatabase.child("posts").child(key).setValue(newPost);
+
+        // Read from the database upon adding new entry (UPDATE POSTINGS HERE)
+        mDatabase.child("posts").child(key).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    Post data = dataSnapshot.getValue(Post.class);
+
+//                    LOG STATEMENTS FOR DEBUGGING
+//                    Log.d("Success", "userid is: " + data.getUserID());
+//                    Log.d("Success", "desc is: " + data.getDescription());
+//                    Log.d("Success", "status is: " + data.getStatus());
+//                    Log.d("Success", "date is: " + data.getDate());
+//                    Log.d("Success", "latitude is: " + data.getLatitude());
+//                    Log.d("Success", "longitude is: " + data.getLongitude());
+//                    Log.d("Success", "imageurl is: " + data.getImageURL());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Error", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
     private void socialMediaUpload() {
         Intent mediaUpload = new Intent();
         mediaUpload.setAction(Intent.ACTION_SEND);
@@ -249,6 +314,17 @@ public class MainActivity extends AppCompatActivity {
     private void loadPager(){
         Intent intent = new Intent(MainActivity.this, PostActivity.class);
         startActivity(intent);
+    }
+
+    private String getFirebaseUserID(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid;
+        if (user != null) {
+            uid = user.getUid();
+        } else {
+            uid = "guest";
+        }
+        return uid;
     }
 
 //    private void socialMediaUpload() {
